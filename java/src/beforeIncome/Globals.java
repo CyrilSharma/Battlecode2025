c06d0d50b35e80b88ad66ea31fe7cf7924f60a2b
CyrilSharma <sharma.cyril@gmail.com>
2025-01-24 18:55:49 -0500

    Before Income

---
+++ java/src/beforeIncome/Globals.java
@@ -0,0 +1,81 @@
+package beforeIncome;
+import java.util.Random;
+import battlecode.common.*;
+
+public class Globals {
+    public static int roundNum;
+    public static RobotController rc;
+    public static final Random rng = new Random();
+    public static boolean[][] moneyTower;
+    public static boolean[][] paintTower;
+    public static void init(RobotController rc) throws GameActionException {
+        rng.setSeed((long) rc.getID());
+        Globals.rc = rc;
+        moneyTower = rc.getTowerPattern(UnitType.LEVEL_ONE_MONEY_TOWER);
+        paintTower = rc.getTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER);
+        Globals.born = rc.getRoundNum();
+    }
+
+    public static RobotInfo[] friends;
+    public static RobotInfo[] enemies;
+    public static void run() throws GameActionException {
+        friends = rc.senseNearbyRobots(-1, rc.getTeam());
+        enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+        roundNum = rc.getRoundNum();
+        updateIncomeTracker();
+    }
+
+    public static boolean onMap(MapLocation loc) throws GameActionException {
+        return (loc.x >= 0 && loc.y >= 0 && loc.x < rc.getMapWidth() && loc.y < rc.getMapHeight());
+    }
+
+    public static UnitType getTowerToBuild(MapLocation loc) throws GameActionException {
+        MapLocation paintMark = loc.add(Direction.WEST);
+        MapLocation moneyMark = loc.add(Direction.SOUTH);
+        if (rc.canSenseLocation(paintMark)){
+            MapInfo s = rc.senseMapInfo(paintMark);
+            if (s.getMark() == PaintType.ALLY_SECONDARY) return UnitType.LEVEL_ONE_PAINT_TOWER;
+        }
+        if( rc.canSenseLocation(moneyMark)) {
+            MapInfo s = rc.senseMapInfo(moneyMark);
+            if (s.getMark() == PaintType.ALLY_SECONDARY) return UnitType.LEVEL_ONE_MONEY_TOWER;
+        }
+        if (rc.getMoney() < 2000) return UnitType.LEVEL_ONE_MONEY_TOWER;
+        return UnitType.LEVEL_ONE_PAINT_TOWER;
+    }
+
+    public static boolean isTower(UnitType u) {
+        return switch (u) {
+            case SOLDIER -> false;
+            case SPLASHER -> false;
+            case MOPPER -> false;
+            default -> true;
+        };
+    }
+
+
+     // Income Tracking.
+    public static int born;
+    public static int income = GameConstants.NUMBER_INITIAL_MONEY_TOWERS * UnitType.LEVEL_ONE_MONEY_TOWER.moneyPerTurn;
+    public static int lastMoney = 0;
+    public static int moneyUpdateFreq = 30;
+    public static void updateIncomeTracker() throws GameActionException {
+        if (roundNum < moneyUpdateFreq) return;
+        int modulo = (roundNum % moneyUpdateFreq);
+        if (modulo == 0) {
+            lastMoney =  rc.getMoney();
+        } else if (modulo == 1) {
+            income = (rc.getMoney() - lastMoney);
+        }
+    }
+
+    public static boolean shouldStallForIncome() throws GameActionException {
+        if (roundNum < moneyUpdateFreq) return false;
+        int modulo = (roundNum % moneyUpdateFreq);
+        return (modulo == 0) || (modulo == 1);
+    }
+    
+    public static int getIncome() {
+        return income;
+    }
+}
---
+++ java/src/beforeIncome/RobotPlayer.java
@@ -0,0 +1,98 @@
+package beforeIncome;
+import battlecode.common.*;
+public class RobotPlayer {
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+        
+        Globals.init(rc);
+        Mopper.init(rc);
+        Pathing.init(rc);
+        Soldier.init(rc);
+        Splasher.init(rc);
+        Tower.init(rc);
+        TileLoader.init(rc);
+        SymmetryChecker.init(rc);
+        Attack.init(rc);
+        MopperAttack.init(rc);
+        Explore.init(rc);
+        RefuelManager.init(rc);
+        TowerBuild.init(rc);
+        SquareManager.init(rc);
+        Comms.init(rc);
+        switch (rc.getType()) {
+            case SOLDIER: {
+                while (true) {
+                    try {
+                        TileLoader.load();
+                        Globals.run();
+                        SquareManager.run();
+                        Soldier.run();
+                        SymmetryChecker.run();
+                        Comms.run();
+                        // rc.setIndicatorString("Symmetry: R = " + SymmetryChecker.RSYM + " V = " + SymmetryChecker.VSYM + " H = " + SymmetryChecker.HSYM);
+                    } catch (GameActionException e) {
+                        System.out.println("Exception = ");
+                        e.printStackTrace();
+                    } finally {
+                        Clock.yield();
+                    }
+                }
+            }
+            case MOPPER: {
+                while (true) {
+                    try {
+                        TileLoader.load();
+                        Globals.run();
+                        SquareManager.run();
+                        Mopper.run();
+                        SymmetryChecker.run();
+                        Comms.run();
+                        // rc.setIndicatorString("Symmetry: R = " + SymmetryChecker.RSYM + " V = " + SymmetryChecker.VSYM + " H = " + SymmetryChecker.HSYM);
+                    } catch (GameActionException e) {
+                        System.out.println("Exception = ");
+                        e.printStackTrace();
+                    } finally {
+                        Clock.yield();
+                    }
+                }
+            }
+            case SPLASHER: {
+                while (true) {
+                    try {
+                        TileLoader.load();
+                        Globals.run();
+                        SquareManager.run();
+                        Splasher.run();
+                        SymmetryChecker.run();
+                        Comms.run();
+                        // rc.setIndicatorString("Symmetry: R = " + SymmetryChecker.RSYM + " V = " + SymmetryChecker.VSYM + " H = " + SymmetryChecker.HSYM);
+                    } catch (GameActionException e) {
+                        System.out.println("Exception = ");
+                        e.printStackTrace();
+                    } finally {
+                        Clock.yield();
+                    }
+                }
+            }
+            
+            default: {
+                while (true) {
+                    try {
+                        TileLoader.load();
+                        Globals.run();
+                        SquareManager.run();
+                        Tower.run();
+                        SymmetryChecker.run();
+                        Comms.run();
+                        // rc.setIndicatorString("Symmetry: R = " + SymmetryChecker.RSYM + " V = " + SymmetryChecker.VSYM + " H = " + SymmetryChecker.HSYM);
+                    } catch (GameActionException e) {
+                        System.out.println("Exception = ");
+                        e.printStackTrace();
+                    } finally {
+                        Clock.yield();
+                    }
+                }
+            }
+        }
+    }
+}
